743eec8bd905b4302c637fecdb8fa4ab0b0793ea
SON-HYUN <sctoa1@gmail.com>
2023-05-21 16:59:34 +0900

    urls.py : '/import' 추가

--- food/foodoctor/urls.py
+++ food/foodoctor/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import base_views
+from .views import base_views, db_control
 
 app_name = 'foodoctor'
 
 urlpatterns = [
     path('', base_views.index, name='index'),
+
+    # json 으로 된 식당 정보를 DB에 추가하는 db_control.temp() 실행
+    # 현재 .save() 하는 코드를 temp()에 넣진 않음
+    path('import/', db_control.temp, name='import')
 ]
