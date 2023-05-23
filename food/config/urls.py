1cd7af525cdafb764b29da1378dda72102b864ea
SON-HYUN <sctoa1@gmail.com>
2023-05-23 15:57:54 +0900

    urls.py : korean_food url 연결

--- food/config/urls.py
+++ food/config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from foodoctor.views import base_views
+from ..foodoctor.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
